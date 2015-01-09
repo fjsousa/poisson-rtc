85621591eb859e54837d3aaa82ae8118df1c2940
Flávio Sousa <flavio.jsousa@gmail.com>
2015-01-09 16:30:36 +0000

    Added tests

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ var Poisson = require('./lib/poisson');
 var conditions = {
   w: 1,
   h: 1,
-  n: 300, //y # of nodes
-  m: 300 //x # of nodes
+  n: 100, //y # of nodes
+  m: 100 //x # of nodes
 }
 
 var poisson = new Poisson(conditions);
 
-var maxItterations = 100000;
-var maxResidue = 1E-5 ;
+var maxItterations = 1000000;
+var maxResidue = 1E-9 ;
 poisson.solver( maxItterations, maxResidue);
 
-poisson.print('./field.txt');
+poisson.analitical();
+
+poisson.print('./field.txt', poisson.u.new);
+poisson.print('./analitical.txt', poisson.u.analitical);
 
 
 
