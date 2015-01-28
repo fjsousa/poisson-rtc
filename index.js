2b08bcace4826504a2fc7cd799b6266a2f3cf032
Flávio Sousa <flavio.jsousa@gmail.com>
2015-01-28 00:09:37 +0000

    Finished server

---
+++ index.js
@@ -0,0 +1,42 @@
+
+var PeerServer = require('peer').PeerServer;
+var WebSocketServer = require('ws').Server;
+
+//Number of blocks
+var blockRows = 2;
+var blockCols = 2;
+var nd = blockRows*blockCols;
+
+var peerServer = PeerServer({port: 9000, path: '/myapp'});
+var wss = new WebSocketServer({port: 9001});
+var peerList = new Array();
+var masterSocket = null;
+
+//Listen for peer connections
+peerServer.on('connection', function (id) {
+  console.log('Peer connected with id:', id);
+});
+
+
+//Listen for ws connections
+wss.on('connection', function(socket) {
+  socket.on('message', function(msg){
+    msg = JSON.parse(msg);
+
+    if (msg.signal ==='id'){
+      peerList.push(msg.id);
+
+      //First socket is master socket
+      if (!masterSocket){
+        masterSocket = socket;
+      }
+
+    }
+
+    if ( peerList.length === (nd + 1)) {
+      console.log('Launching Master...')
+      masterSocket.send(JSON.stringify({peerList: peerList, blockRows: blockRows, blockCols: blockCols}));
+    }
+
+  });
+});
